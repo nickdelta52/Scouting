1ce7cde1e7d04c95343a439bf3d1d1c62f48918b
ghana7 <ghana796.2>
2016-01-24 14:55:37 -0500

    Added Team Class and dank memes

---
+++ Scouting/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scouting
+{
+    public class Team
+    {
+        public int number;
+        public Team(int _number)
+        {
+            this.number = _number;
+        }
+    }
+}
