d3de5b62567af1a364a3e1ba99260361c6f25bc5
nickdelta52 <nfbdiamond@gmail.com>
2016-01-26 18:38:36 -0500

    Added show all button

--- Scouting/MainForm.aspx.designer.cs
+++ Scouting/MainForm.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace Scouting {
         protected global::System.Web.UI.WebControls.Button btnView;
         
         /// <summary>
+        /// btnShowAll control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnShowAll;
+        
+        /// <summary>
         /// parent control.
         /// </summary>
         /// <remarks>
